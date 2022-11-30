d857540e89bdf71bc07f6838d837dd8cd9ea5534
Cassandradlm <cassandra.dallemagne@esme.fr>
2022-11-30 14:27:01 +0100

    first push

--- app/src/main/java/com/example/tpgr5msi/MainActivity.java
+++ app/src/main/java/com/example/tpgr5msi/MainActivity.java
@@ -1,26 +1,22 @@
 package com.example.tpgr5msi;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.tpgr5msi.databinding.ActivityMainBinding;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
@@ -30,47 +26,61 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+/*
+        try {
+            JSONObject json = new JSONObject();
+            json.put("id",39);
+            json.put("name","Pomme");
+            json.put("type","Fruit");
+            json.put("price",10.0);
+            ConnectionRest connectionRest = new ConnectionRest();
+            //connectionRest.setJsonObj(json);
+            connectionRest.execute("GET");
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }*/
+        ArrayList<Product> listData = getListData();
+        final ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new CustomListAdapter(this, listData));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                Object o = listView.getItemAtPosition(position);
+                Product upload = (Product) o;
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                intent.putExtra("id", upload.getId());
+                intent.putExtra("name", upload.getName());
+                intent.putExtra("type", upload.getType());
+                intent.putExtra("price", upload.getPrice());
+                startActivity(intent);
+            }
+        });
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                startActivity(intent);
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public ArrayList<Product> getListData(){
+        try{
+            ConnectionRest connectionRest = new ConnectionRest();
+            connectionRest.execute("GET");
+            String listJsonObjs = connectionRest.get();
+            if(listJsonObjs != null) {
+                return connectionRest.parse(listJsonObjs);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
-
-        return super.onOptionsItemSelected(item);
+        return null;
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
